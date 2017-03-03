e543d57b6b5f054b6d50e92b55aae4262c8c0305
Nummo <Nenna@Nennas-MacBook-Pro.local>
2017-03-03 16:38:31 +0000

    added basic files

---
+++ footer.php
@@ -0,0 +1,12 @@
+    </div> <!-- /.container -->
+
+		<footer class="blog-footer">
+      <p>Faith Blog </p>
+      <p>
+        <a href="#">Back to top</a>
+      </p>
+    </footer>
+
+<?php wp_footer(); ?> 
+  </body>
+</html>
