33e8987ee8ad16a407ef2d1719f7c664d6780678
Nen <nenna.chuku@gmail.com>
2017-04-11 17:26:37 +0100

    header changes

--- index2.php
+++ index2.php
@@ -1,33 +1,20 @@
 <?php
 /**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package WordPress
- * @subpackage Twenty_Seventeen
- * @since 1.0
- * @version 1.0
+
  */
 
 get_header(); ?>
 
 <div class="wrap">
 	<?php if ( is_home() && ! is_front_page() ) : ?>
-		<header class="page-header">
-			<h1 class="page-title"><?php single_post_title(); ?></h1>
+		<header class="entry-header">
+			<h1><?php single_post_title(); ?></h1>
 		</header>
 	<?php else : ?>
-	<header class="page-header">
-		<h2 class="page-title"><?php _e( 'Posts', 'twentyseventeen' ); ?></h2>
+	<header class="entry-header">
+		<h1 ><?php _e( 'Posts', 'twentyseventeen' ); ?></h1>
 	</header>
 	<?php endif; ?>
-		
 	
 		<main id="main" class="site-main" role="main">
 	<div id="masonry-loop">
