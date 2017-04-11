cd09eb4b3f8bfbaf0135c8a3ced2cb3b7686eb6a
Nen <nenna.chuku@gmail.com>
2017-04-11 09:49:06 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,12 +1,65 @@
-    </div> <!-- /.container -->
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
 
-		<footer class="blog-footer">
-      <p>Faith Blog </p>
-      <p>
-        <a href="#">Back to top</a>
-      </p>
-    </footer>
+?>
+if ( ! function_exists( 'slug_masonry_init' )) :
+function slug_masonry_init() { ?>
+<script>
+    //set the container that Masonry will be inside of in a var
+    var container = document.querySelector('#masonry-loop');
+    //create empty var msnry
+    var msnry;
+    // initialize Masonry after all images have loaded
+    imagesLoaded( container, function() {
+        msnry = new Masonry( container, {
+            itemSelector: '.masonry-entry'
+        });
+    });
+</script>
+<?php }
+//add to wp_footer
+add_action( 'wp_footer', 'slug_masonry_init' );
+endif; // ! slug_masonry_init exists
 
-<?php wp_footer(); ?> 
-  </body>
+		</div><!-- #content -->
+
+		<footer id="colophon" class="site-footer" class="aatc-footer" role="contentinfo">
+			<div class="wrap">
+				<?php
+				get_template_part( 'template-parts/footer/footer', 'widgets' );
+
+				if ( has_nav_menu( 'social' ) ) : ?>
+					<nav class="social-navigation" role="navigation" aria-label="<?php _e( 'Footer Social Links Menu', 'twentyseventeen' ); ?>">
+						<?php
+							wp_nav_menu( array(
+								'theme_location' => 'social',
+								'menu_class'     => 'social-links-menu',
+								'depth'          => 1,
+								'link_before'    => '<span class="screen-reader-text">',
+								'link_after'     => '</span>' . twentyseventeen_get_svg( array( 'icon' => 'chain' ) ),
+							) );
+						?>
+					</nav><!-- .social-navigation -->
+				<?php endif;
+
+				get_template_part( 'template-parts/footer/site', 'info' );
+				?>
+			</div><!-- .wrap -->
+		</footer><!-- #colophon -->
+	</div><!-- .site-content-contain -->
+</div><!-- #page -->
+<?php wp_footer(); ?>
+
+</body>
 </html>
