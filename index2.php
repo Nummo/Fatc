ce175f061c47498b519fa90f21aa2bd42a7587ba
Nen <nenna.chuku@gmail.com>
2017-04-11 10:20:50 +0100

    removed <div id="primary" class="content-area"> on 31

--- index2.php
+++ index2.php
@@ -27,22 +27,22 @@ get_header(); ?>
 		<h2 class="page-title"><?php _e( 'Posts', 'twentyseventeen' ); ?></h2>
 	</header>
 	<?php endif; ?>
-
-	<div id="primary" class="content-area">
+		
+	
 		<main id="main" class="site-main" role="main">
-
+	<div id="masonry-loop">
 			<?php
 			if ( have_posts() ) :
 
 				/* Start the Loop */
 				while ( have_posts() ) : the_post();
-
+	
 					/*
 					 * Include the Post-Format-specific template for the content.
 					 * If you want to override this in a child theme, then include a file
 					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 					 */
-					get_template_part( 'template-parts/post/content', get_post_format() );
+					get_template_part( 'template-parts/post/content', 'masonry' );
 
 				endwhile;
 
@@ -54,7 +54,7 @@ get_header(); ?>
 
 			else :
 
-				get_template_part( 'template-parts/post/content', 'none' );
+				get_template_part( 'template-parts/post/content', 'masonry' );
 
 			endif;
 			?>
