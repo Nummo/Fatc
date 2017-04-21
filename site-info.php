d0644046ac00e07efc13e5483268399b611c8d4b
Nen <nenna.chuku@gmail.com>
2017-04-21 13:11:43 +0100

    Create site-info.php

---
+++ site-info.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div >
+<table id= "tablefooter">
+<div id="footer">
+<tr class="addresses">
+  <td> </td>
+   <td> Faith UK </td>
+   <td> Faith Sansthan </td>
+   </tr>
+
+   <tr class="addresses">
+   <td> </td>
+   <td> Arms Around the Child </td>
+   <td> 31, Palm Court  </td>
+   </tr>
+
+   <tr class="addresses">
+   <td align="left"> Supported by <a target="_blank"  href ="http://armsaroundthechild.org" >Arms Around the Child</a></td>
+   <td> 4th floor, 81 Piccadilly </td>
+   <td> Jagatpura</td>
+   </tr>
+
+
+    <tr class="addresses">
+     <td align="left"> © Copyright 2017 | Faith | All rights reserved</td>
+    <td>London</td>
+    <td> Jaipur, Rajasthan </td>
+    </tr>
+    
+    <tr class="addresses">
+    <td> </td>
+    <td>W1J 8JY</td>
+    <td> 302025 India</td>
+    </tr>
+    </table>
+
+</div>
+<!-- .site-info -->
